7ad98d1d1adc89132cb9d22d506f84b858c1ec0b
lab4 <lab4@fatecrl.com>
2016-11-23 20:55:03 -0200

    Exercicio 8 (incompleto)

---
+++ lista5ex8.c
@@ -0,0 +1,45 @@
+// Lista 5 ex 8
+/**
+
+ Crie a função "Delta" que receba da função "main" três números reais que
+ garantem a existência de uma equação do segundo grau.
+
+ Verifique e exiba dentro da função main se há raízes reais e os seus
+ respectivos valores.
+
+ Caso contrário informe ao usuário.
+
+ a. Condição de existência de uma equação do segundo grau: o coeficiente que acompanha o x2 deverá ser um número diferente de zero.
+ b. Delta maior que zero: a equação possui duas raízes reais e distintas.
+ c. Delta igual à zero: a equação possui duas raízes iguais.
+ d. Delta menor que zero: a equação não apresenta raízes reais.
+
+ ENVIAR PARA OCARATECNICO@HOTMAIL.COM
+ */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ void main(){
+
+ }
+
+ void delta(int a, int b, int c){
+
+ }
+
+ /**
+  bloco de notas
+  https://archive.ics.uci.edu/ml/machine-learning-databases/wine/
+  http://flowchart.js.org/
+  http://sebastien.drouyer.com/jquery.flowchart-demo/
+  http://isfaluno.mec.gov.br/
+  http://fatecrledu.kinghost.net/moodle/
+  lojadoseven.com.br/
+  http://slavnik.fe.uni-lj.si/markot/csv2arff/csv2arff.php
+  slavnik.fe.uni-lj.si/
+  https://websai.cps.sp.gov.br/Autenticacao/Default.aspx
+  */
+
+
+
